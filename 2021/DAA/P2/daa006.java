f6ca969d14833455c703baf4e85f86cf5ddb0bae
lol271998 <filipecolladavid@hotmail.com>
2020-10-15 00:42:05 +0100

    PC FILIPE

--- 2021/DAA/P2/daa006.java
+++ 2021/DAA/P2/daa006.java
@@ -6,10 +6,13 @@ public class daa006{
 
 	public static double intersecao(double x1, double y1, double x2,double y2, double l){
 
-		int cis = circleInsideSquare(x1,y1,x2,y2);
+		int cis = circleANDSquare(x1,y1,x2,y2);
 
    		//Completamente fora uma da outra
-   		if(cis == 0) return 0;
+   		if(cis == 0){
+   			//System.out.println("out");
+   			return 0;
+   		}
    		//Quadrado dentro do Circulo
    		if(cis == 1) return l*l;
    		//Circulo dentro do Circulo
@@ -18,46 +21,71 @@ public class daa006{
    		double area = 0.0000;
    		if(l>=0.001){
 
-   			//System.out.println("Recursion");
-
-   			area = area + intersecao(x1,y1+(l/2),x1+(l/2),y2,(l/2));
-			area = area + intersecao(x1+(l/2),y1+(l/2),x2,y2,(l/2));
-			area = area + intersecao(x1,y1,x1+(l/2),y1+(l/2),(l/2));
-			area = area + intersecao(x1+(l/2),y1,x2,y1+(l/2),(l/2));
+   									//Quadrado inferior direito
+   			area = area + intersecao(x1,y1+(l/2.0),x1+(l/2.0),y2,(l/2.0));
+   									//Quadrado superior direito
+			area = area + intersecao(x1+(l/2.0),y1+(l/2.0),x2,y2,(l/2.0));
+									//Quadrado inferior esquerdo
+			area = area + intersecao(x1,y1,x1+(l/2.0),y1+(l/2.0),(l/2.0));
+									//Quadrado superior esquerdo
+			area = area + intersecao(x1+(l/2.0),y1,x2,y1+(l/2.0),(l/2.0));
 		}
 
 		return area;
 	}
 
-	public static int circleInsideSquare(double x1, double y1, double x2, double y2){
+	public static int CircleANDSquare(double x1, double y1, double x2, double y2){
 		//x2>x1; y2>y1;
 
-		//Circle inside 
+		/*Circle inside 
 		System.out.println("(x1,y1): ("+x1+","+y1+")");
 		System.out.println("(x2,y2): ("+x2+","+y2+")");
 		System.out.println("(cx,cy): ("+cx+","+cy+") Raio -> "+cr);
-		
+		*/
 
-		//Completamente fora
-		if(cx-cr>x2 || cy-cr>y2 || cy+cr<y1 || cx+cr<x1){
-			System.out.println("Completamente fora");
-			return 0;
-		} 
 		//Quadrado dentro
-		if(cx+cr >= x2 && cy+cr >= y2 && cx-cr<=x1 && cy-cr<=y1){
-			System.out.println("Quadrado dentro");
+		//Calcular qual o ponto mais longe do centro
+		Double dx = Math.max(cx - x1, x2 - cx); 
+		Double dy = Math.max(cy - y2, y1 - cx);
+
+		if(cr*cr >= dx*dx + dy*dy){
+			//System.out.println("Quadrado Dentro");
 			return 1;
 		}
+
 		//Ciruclo Dentro
-		if(cx+cr <= x2 && cy+cr <= y2 && cx-cr>=x1 && cy-cr>=y1){
-			System.out.println("Ciruclo Dentro");
+		if(cx+cr <= x2 && cy+cr <= y2 && cx-cr >= x1 && cy-cr >= y1){
+			//System.out.println("Ciruclo Dentro");
 			return 2;
 		}
 
-		else{
-			//System.out.println("Else");
-			return 3;
+		//1º Quadrante
+		if(x1>=cx && y1>=cy){
+			double h2 = ((x1-cx)*(x1-cx)) + ((y1-cy)*(y1-cy));
+			if(Math.sqrt(h2)>=cr) return 0;
 		}
+
+		//2º Quadrante
+		if(x2<=cx && y1>=cy){
+			double h2 = ((cx-x2)*(cx-x2)) + ((y1-cy)*(y1-cy));
+			if(Math.sqrt(h2)>=cr) return 0;
+		}
+
+		//3º Quadrante
+		if(x2<=cx && y2<=cy){
+			double h2 = ((cx-x2)*(cx-x2)) + ((cy-y2)*(cy-y2));
+			if(Math.sqrt(h2)>=cr) return 0;
+		}
+
+		//4º Quadrante
+		if(x1<=cx && y2>=cy){
+			double h2 = ((x1-cx)*(x1-cx)) + ((cy-y2)*(cy-y2));
+			if(Math.sqrt(h2)>=cr) return 0;
+		}
+
+		//System.out.println("Not inside, Nor Outside.");
+		return 3;
+			
 	}
 
 	public static void main(String[] args) {
