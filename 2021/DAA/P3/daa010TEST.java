5d884d679f233103ef7617b3b41095d100b0a0ec
lol271998 <filipecolladavid@hotmail.com>
2020-10-28 18:39:25 +0000

    PC FILIPE

--- 2021/DAA/P3/daa010TEST.java
+++ 2021/DAA/P3/daa010TEST.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
-public class daa010{
-
+public class daa010TEST{
+    
     public static int binarysearch(int[] v, int low, int high, int key){
         
         if(key>v[high]) return high;
@@ -11,18 +11,17 @@ public class daa010{
         while(low < high){
 
             int middle = low + (high-low)/2;
-
             if(v[low] == key) return low;
             if(v[middle] == key) return middle;
             if(v[high] == key) return high;
 
-            /*
+            
             System.out.println("Key: "+key);
             System.out.println("low: v["+low+"]: "+v[low]);
             System.out.println("middle: v["+middle+"]: "+v[middle]);
             System.out.println("high: v["+high+"]: "+v[high]);
             System.out.println("--------------------------------");
-			*/
+
             if(key-v[middle] == v[middle+1]-key){
                 System.out.print(v[middle]+" ");
                 low = middle + 1;
@@ -32,60 +31,62 @@ public class daa010{
             if(Math.abs(key-v[low]) > Math.abs(key-v[middle])) low = middle+1;
             else if(Math.abs(key-v[low]) <= Math.abs(key-v[middle])){
                 if(Math.abs(key-v[low])>Math.abs(key-v[high])) return high;
-                else high = middle+1;
+                else high = middle-1;
             }
-
-            /*
             System.out.println("Key: "+key);
             System.out.println("low: v["+low+"]: "+v[low]);
             System.out.println("middle: v["+middle+"]: "+v[middle]);
             System.out.println("high: v["+high+"]: "+v[high]);
             System.out.println("--------------------------------");
             System.out.println("--------------------------------");
-            */
         }
         return low;
     }
 
+    public static int fact(int n){
+        int res = 1;
 
-	public static int fact(int n){
-		int res = 1;
+        for(int i = n-1; i>0; i--){
+            res=res*n;
+            n--;
+        }
+        return res;
+    }
 
-		for(int i = n-1; i>0; i--){
-			res=res*n;
-			n--;
-		}
-		return res;
-	}
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+        int[] v =  {7,24,18,25,49,17,30,26,14,47};
+        int n = v.length;
+        int nPairs = fact(n)/(2*(fact(n-2)));
+        int[] somas = new int[nPairs];
+        int k = 0;
 
-		int n = in.nextInt();
-		int v[] = new int[n];
+        for(int i = 0; i<n-1; i++){
+            for(int j = i+1; j<n; j++){ 
+                somas[k] = v[i]+v[j];
+                k++;
+            }
+        }
 
-		int nPairs = fact(n)/(2*(fact(n-2)));
-		int somas[] = new int[nPairs];
+        System.out.println("");
 
-		for(int i = 0; i<n; i++) v[i] = in.nextInt();
+        Arrays.sort(somas);
 
-		int k = 0;
+        for(int i = 0; i<k; i++){
+            if(i == 0)System.out.print("{"+somas[i]);
+            else if(i==k-1) System.out.print(","+somas[i]+"}");
+            else System.out.print(","+somas[i]);
+        }
 
-		for(int i = 0; i<n-1; i++){
-			for(int j = i+1; j<n; j++){	
-				somas[k] = v[i]+v[j];
-				k++;
-			}
-		}
-		
-		Arrays.sort(somas);
+        System.out.println("");
 
-		int q = in.nextInt();
-		for(int i = 0; i<q; i++){
-			int qnum = in.nextInt();
-			int a = binarysearch(somas,0,k-1,qnum);
-			System.out.println(somas[a]);
-		}
+        int[] q = {39};
+        
+        for(int i = 0; i<1; i++){
 
-	}
+            int a = binarysearch(somas,0,k-1,39);
+            System.out.println(somas[a]);
+        }
+
+    }
 }
