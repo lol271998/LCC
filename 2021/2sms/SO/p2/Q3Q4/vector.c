d39b8b9d01c168a0658191d7533a4dcdbc4ca218
FilipeDavid <lol271998@gmail.com>
2021-05-06 15:41:21 +0100

    PC FILIPE

--- 2021/2sms/SO/p2/Q3Q4/vector.c
+++ 2021/2sms/SO/p2/Q3Q4/vector.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <math.h>
 
 #include "vector.h"
 /*
@@ -14,28 +15,31 @@ vector* vector_new(double x,double y,double z) {
 }
 
 vector* vector_add(vector* v1, vector* v2) {
-	return v1;
+	vector* v3 = vector_new(v1->x + v2->x,v1->y + v2->y,v1->z + v2->z);
+	return v3;
 }
 
 vector* vector_sub(vector* v1, vector* v2) {
-	return v1;
+	vector *v3 = vector_new(v1->x - v2->x, v1->y - v2->y, v1->z - v2->z);
+	return v3;
 }
 
 vector* vector_scale(double d, vector* v) {
-	return v;
+	vector *u = vector_new(v->x*d,v->y*d,v->z*d);
+	return u;
 }
 
 vector* vector_vprod(vector* v1, vector* v2) {
+	//double v = vector_sprod(v1,v2);
 	return v1;
 }
 
 double vector_sprod(vector* v1, vector* v2) {
-	return 0.1;
+	double r = v1->x*v2->x + v1->y*v2->y + v1->z*v2->z; 
+	return r;
 }
 
-double vector_mod(vector* v1) {
-	return 0.1;
-}
+double vector_mod(vector* v1) {	return sqrt(v1->x*v1->x+v1->y*v1->y); }
 
 
 
