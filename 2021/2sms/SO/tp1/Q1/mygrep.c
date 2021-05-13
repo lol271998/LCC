e5b81cf98369a4796cdd2b717f6951bc0d043d49
FilipeDavid <lol271998@gmail.com>
2021-05-13 12:58:21 +0100

    PC FILIPE

---
+++ 2021/2sms/SO/tp1/Q1/mygrep.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[]) {
+
+	if(argc < 2) {
+		printf("Error: Not enough arguments \n");
+		return 1;
+	}
+	char* line = NULL;
+	size_t n = 0;
+	int line_count = 0;
+	FILE *fp = fopen(argv[1],"r");
+	
+	const char* needle = argv[2];
+
+	while ((getline(&line,&n,fp)) != -1) {
+		//printf("n: %ld\n",n);
+		line_count++;
+		char* str = strstr(line,needle);
+		
+		if(str!=NULL) {
+			printf("[%d:%ld",line_count,(strlen(line)-(strlen(str))+1));
+			str++;
+			str = strstr(str,needle);
+			while(str!=NULL) {
+				printf(",%ld",(strlen(line) - strlen(str)+1));
+				str++;
+				str = strstr(str,needle);
+			}
+			printf("]\n");
+		}
+
+		free(str);
+		str = NULL;
+		free(line);
+		n = 0;
+		line = NULL;
+	}
+
+	return 0;
+}
+/*
+n = getline; //n = numero de caracteres;
+strtok;
+strlen;
+strcmp;
+strstr;
+*/
