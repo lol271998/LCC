af2b6f2dcb90715f1c5ac13f3bdf2f05d55cb1ff
lol271998 <filipecolladavid@hotmail.com>
2021-05-19 23:22:21 +0100

    PC FILIPE

--- 2021/2sms/SO/p5/q1.c
+++ 2021/2sms/SO/p5/q1.c
@@ -2,19 +2,19 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
-
 int main(int argc, char* argv[]) {
-	
 	int i, wstatus;
-	
+
 	/* fork a child process */
 	printf("%d\n", fork());
 
 	/* fork another child process */
 	printf("%d\n", fork());
 
-	/* and fork another */ 
+	/* and fork another */
 	printf("%d\n", fork());
 
+	for(i=0; i<3; i++)
+		wait(&wstatus);
 	return EXIT_SUCCESS;
 }
--- 2021/2sms/SO/tp1/Q2/mapreduce.c
+++ 2021/2sms/SO/tp1/Q2/mapreduce.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <errno.h>
 
 int main(int argc, char const *argv[]) {
 
@@ -8,10 +13,42 @@ int main(int argc, char const *argv[]) {
   }
 
   FILE *fp = fopen(argv[1],"r");
+  int n = 0,start,end;
 
   int m = atoi(argv[2]);
+  int parcials[m];
+  int fd[2];
   int k = atoi(argv[3]);
+ 
+  fscanf(fp,"%d",&n);
 
-  
+  if(pipe(fd) == -1) {
+    return 1;
+  }
+  int arr[n*n];
+  for(int i = 0; i<n*n; i++) {
+    fscanf(fp,"%d",&arr[i]);
+    //printf("%d: %d\n",i,arr[i]);
+  }
+
+  for(int i = 0; i<m/2; i++) {
+    pid_t id = fork();
+    if(id == -1) return 2;
+    //Proceso filho
+    if(id == 0) {
+      start = 0;
+      end = (n*n)/2;
+    } else {
+      start = (n*n) / 2;
+      end = (n*n);
+    }
+
+    int sum = 0;
+    int i;
+    for (i = start; i < end; i++) {
+      sum+=arr[i];
+    }
+    printf("Calculated partial sum: %d\n", sum);
+  }
   return 0;
 }
