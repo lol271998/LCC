a5a86fb5487ea3678e0314338dbf4ba2f520e6d6
FilipeDavid <lol271998@gmail.com>
2021-05-21 10:34:43 +0100

    PC FILIPE

---
+++ 2021/2sms/IA/T2/tp1.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+f = pd.read_csv("speedDating_trab.csv")
+
+print(f.head(100))
--- 2021/2sms/SO/tp1/Q2/mapreduce.c
+++ 2021/2sms/SO/tp1/Q2/mapreduce.c
@@ -3,7 +3,37 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/mman.h>
 #include <errno.h>
+
+void do_work(int np,const char* fname, int m, int k, int* parciais) {
+  
+  FILE *fp = fopen(fname,"r");
+  if(fp == NULL) {
+    printf("Cannot open file \n");
+    return;
+  }
+
+  char* line = NULL;
+
+  int size = 4;
+  fscanf(fp,"%d",&size); //usar getline
+  fgetc(fp); //'\n'
+  for(int i = 0; i<size; i++) {
+    getline(&line,NULL,fp);
+    if(i%m == np) {
+      for(int j = 0; j<size; j++) {
+        char* tok;
+        tok = strtok(line," ");
+        while(tok != NULL) {
+          if(k<atoi(tok)) parciais[i]++;
+          tok = strtok(NULL," ");
+        }
+      }
+    }
+  }
+  fclose(fp);
+}
 
 int main(int argc, char const *argv[]) {
 
@@ -11,44 +41,31 @@ int main(int argc, char const *argv[]) {
     printf("Not enough or too many arguments \n");
     return 1;
   }
+  int m = atoi(argv[2]),k = atoi(argv[3]);
+  
+  int* parciais = (int*)mmap(NULL,m*sizeof(int),PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+  for(int i = 0; i<m; i++)parciais[i] = 0;
 
-  FILE *fp = fopen(argv[1],"r");
-  int n = 0,start,end;
-
-  int m = atoi(argv[2]);
-  int parcials[m];
-  int fd[2];
-  int k = atoi(argv[3]);
- 
-  fscanf(fp,"%d",&n);
-
-  if(pipe(fd) == -1) {
-    return 1;
-  }
-  int arr[n*n];
-  for(int i = 0; i<n*n; i++) {
-    fscanf(fp,"%d",&arr[i]);
-    //printf("%d: %d\n",i,arr[i]);
-  }
-
-  for(int i = 0; i<m/2; i++) {
-    pid_t id = fork();
-    if(id == -1) return 2;
-    //Proceso filho
-    if(id == 0) {
-      start = 0;
-      end = (n*n)/2;
-    } else {
-      start = (n*n) / 2;
-      end = (n*n);
+  for(int i = 1; i<m; i++) {
+    if(fork() == 0) {
+      do_work(i,argv[1],m,k,parciais);
+      exit(0);
     }
-
-    int sum = 0;
-    int i;
-    for (i = start; i < end; i++) {
-      sum+=arr[i];
-    }
-    printf("Calculated partial sum: %d\n", sum);
   }
-  return 0;
+  int sum = 0;
+  for(int i = 0; i<m; i++) {
+    sum+=parciais[i];
+  }
+  printf("%d\n",sum);
 }
+/*
+  if 1.mod.m == nr processo filho
+  for(int i = 0; i<P; i++) {
+    if(fork() == 0) {
+      do_word(...);
+      exit(...);
+    }
+  }
+  int* parciais = (int*)mmap(NULL,m*sizeof(int),PROT_READ | PORT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1,0);
+
+*/
